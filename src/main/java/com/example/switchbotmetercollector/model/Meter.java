fbb601bff81a0427e5ddaaa788387894e258b246
kokano731 <you@example.com>
2022-10-01 14:22:29 +0900

    publish message to pub/sub

--- src/main/java/com/example/switchbotmetercollector/model/Meter.java
+++ src/main/java/com/example/switchbotmetercollector/model/Meter.java
@@ -1,7 +1,7 @@
 package com.example.switchbotmetercollector.model;
 
-// exec_time,device_name,temperature,humidity
-// 2022-07-30 00:04:06,north_room,27.4,57
+// exec_time,device_name,temperature,humidity,dt
+// 2022-07-30 00:04:06,north_room,27.4,57,2022-09-24
 public record Meter (
-    String id, String exec_time, String device_name, Float temperature, Integer humidity ) {
+    String id, String exec_time, String device_name, Float temperature, Integer humidity, String dt ) {
 };
